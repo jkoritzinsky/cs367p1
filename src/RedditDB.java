bee13d9b2c00d1ce2588787a2a87807cac65bea8
Jeremy Koritzinsky <jkoritzinsky@live.com>
2015-01-30 13:34:31 -0600

    Added addUser,findUser,  partial delUser, and partial getFrontPage functionality.

--- src/RedditDB.java
+++ src/RedditDB.java
@@ -13,23 +13,49 @@ public class RedditDB {
 	}
 
 	public User addUser(String name) {
-		//TODO
+		if(findUser(name) == null) {
+			User newUser = new User(name);
+			users.add(newUser);
+			return newUser;
+		}
+		return null;
 	}
 
 	public User findUser(String name) {
-		//TODO
+		if(name == null)
+			throw new IllegalArgumentException("name");
+		for(int i = 0; i < users.size(); ++i){
+			if(users.get(i).getName().equals(name))
+				return users.get(i);
+		}
+		return null;
 	}
 
 	public boolean delUser(String name) {
-		//TODO
+		User userToDelete = findUser(name);
+		if(userToDelete == null) {
+			return false; //TODO: Post on Piazza and figure out exactly what to return
+		}
+		//TODO: Delete all user data recursively
 	}
 
 	public List<Post> getFrontpage(User user) {
-		//TODO
+		if(user == null) {
+			//TODO: Get all posts
+		}
+		List<String> subreddits = user.getSubscribed();
+		List<Post> frontPagePosts = new ArrayList<Post>();
+		for (int i = 0; i < subreddits.size(); ++i) {
+			frontPagePosts.addAll(getFrontpage(user, subreddits.get(i)));
+		}
+		return frontPagePosts;
 	}
 
 	public List<Post> getFrontpage(User user, String subreddit) {
-		//TODO
+		if(user == null) {
+			//TODO: Get all posts for subreddit
+		}
+		//TODO: Get frontpage for user for subreddit
 	}
 }
 
