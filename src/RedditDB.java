4526af198c21dcdc3de3687a1d91450ff90f4f39
Jeremy Koritzinsky <jkoritzinsky@live.com>
2015-02-01 21:18:33 -0600

    Finished RedditDB functionality

--- src/RedditDB.java
+++ src/RedditDB.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -34,28 +35,89 @@ public class RedditDB {
 	public boolean delUser(String name) {
 		User userToDelete = findUser(name);
 		if(userToDelete == null) {
-			return false; //TODO: Post on Piazza and figure out exactly what to return
+			return false;
 		}
-		//TODO: Delete all user data recursively
+		Iterator<Post> postedIter = userToDelete.getPosted().iterator();
+		while(postedIter.hasNext()) {
+			Post post = postedIter.next();
+			Iterator<User> usersIter = users.iterator();
+			while(usersIter.hasNext()) {
+				User user = usersIter.next();
+				user.undoLike(post);
+				user.undoDislike(post);
+			}
+		}
+		Iterator<Post> likedIter = userToDelete.getLiked().iterator();
+		while(likedIter.hasNext()) {
+			Post likedPost = likedIter.next();
+			userToDelete.undoLike(likedPost);
+		}
+		Iterator<Post> dislikedIter = userToDelete.getDisliked().iterator();
+		while(dislikedIter.hasNext()) {
+			Post dislikedPost = dislikedIter.next();
+			userToDelete.undoDislike(dislikedPost);
+		}
+		return true;
 	}
 
 	public List<Post> getFrontpage(User user) {
-		if(user == null) {
-			//TODO: Get all posts
+		List<Post> allPosts = new ArrayList<Post>();
+		Iterator<User> userIter = users.iterator();
+		while(userIter.hasNext()) {
+			allPosts.addAll(userIter.next().getPosted());
 		}
+		if(user == null) return allPosts;
 		List<String> subreddits = user.getSubscribed();
-		List<Post> frontPagePosts = new ArrayList<Post>();
-		for (int i = 0; i < subreddits.size(); ++i) {
-			frontPagePosts.addAll(getFrontpage(user, subreddits.get(i)));
+		List<Post> liked = user.getLiked();
+		List<Post> disliked = user.getDisliked();
+		Iterator<Post> postIter = allPosts.iterator();
+		List<Post> frontpage = new ArrayList<Post>();
+		while(postIter.hasNext()) {
+			Post post = postIter.next();
+			if(subreddits.contains(post.getSubreddit())) {
+				if(post.getUser().equals(user)) {
+					frontpage.add(post);
+				}
+				else {
+					if(!liked.contains(post) && !disliked.contains(post)) {
+						frontpage.add(post);
+					}
+				}
+			}
 		}
-		return frontPagePosts;
+		return frontpage;
 	}
 
 	public List<Post> getFrontpage(User user, String subreddit) {
-		if(user == null) {
-			//TODO: Get all posts for subreddit
+		if(subreddit == null)
+			throw new IllegalArgumentException("subreddit");
+		List<Post> allPosts = getFrontpage(null);
+		Iterator<Post> allPostsIter = allPosts.iterator();
+		List<Post> subredditPosts = new ArrayList<Post>();
+		while(allPostsIter.hasNext()) {
+			Post currentPost = allPostsIter.next();
+			if(currentPost.getSubreddit().equals(subreddit)) {
+				subredditPosts.add(currentPost);
+			}
 		}
-		//TODO: Get frontpage for user for subreddit
+		if(user == null) 
+			return subredditPosts;
+		List<Post> liked = user.getLiked();
+		List<Post> disliked = user.getDisliked();
+		List<Post> frontpage = new ArrayList<Post>();
+		Iterator<Post> subredditIter = subredditPosts.iterator();
+		while(subredditIter.hasNext()) {
+			Post post = subredditIter.next();
+			if(post.getUser().equals(user)) {
+				frontpage.add(post);
+			}
+			else {
+				if(!liked.contains(post) && !disliked.contains(post)) {
+					frontpage.add(post);
+				}
+			}
+		}
+		return frontpage;
 	}
 }
 
